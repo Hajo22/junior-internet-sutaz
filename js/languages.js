af33f77f4da48e7cb4ceb834f0ee17bae4f30e4b
Dávid <101352259+Hajo22@users.noreply.github.com>
2022-03-12 21:30:13 +0100

    Zmena jazyka dokoncena
    Zmena jazykov do Slovenského, Anglického a Ruského na hlavnej stránke dokončená

--- js/languages.js
+++ js/languages.js
@@ -3,19 +3,56 @@
 $(document).ready(function(){
 
     console.log("Načitávanie scriptu jazykov...");
+
     let lang = ["sk", "en", "ru"];
 
-    $("#langEN").click(function(){
-        translateEN();
+    $("select#language").change(function(){
+        var selectedLanguage = $(this).children("option:selected").val();        
+        
+        switch(Number(selectedLanguage)){
+            case 1:
+                translateSK();
+                break;
+            case 2:
+                translateEN();
+                break;
+            case 3:
+                translateRU();
+                break;
+        }
     });
 
-    $("#langRU").click(function(){
-        translateRU();
-    });
+    function translateSK(){
+        console.log("Prekladanie do Slovenčiny...");
+
+        //Navbar
+        $(".langTimeline").text("Časová os");
+        $(".langProject").text("Projekt");
+        $(".langAccount").text("Účet");
+        $(".langLogin").text("Prihlásenie");
+        $(".langRegister").text("Registrácia");
+        $(".langProfile").text("Profil");
+        $(".langLanguage").text("Jazyk");
+        $(".langTheme").text("Téma");
+        $(".langSearch").text("Hľadať");
+        $("#search").attr("placeholder", "Zadajte hľadaný výraz");
+
+        //Casova os
+        $(".langNewPost").text("Vytvoriť príspevok");
+        $("#newPost-textarea").attr("placeholder", "Čo je nové?");
+        $(".langAddPost").text("Pridať");
+        $(".langSavePost").text("Uložiť");
+
+        //Aside
+        $(".aside-panel-title").text("Informácie");
+        $(".langSavedPosts").text("Uložené");
+
+        console.log("Preložené... ");
+    }
 
     function translateEN(){
         console.log("Translating to English...");
-        
+
         //Navbar
         $(".langTimeline").text("Timeline");
         $(".langProject").text("Project");
@@ -31,7 +68,7 @@ $(document).ready(function(){
         //Timeline
         $(".langNewPost").text("Create post");
         $("#newPost-textarea").attr("placeholder", "What's new?");
-        $(".langAddPost").text("Add");
+        $(".langAddPost").text("Send");
         $(".langSavePost").text("Save");
 
         //Aside
@@ -42,9 +79,31 @@ $(document).ready(function(){
     }
 
     function translateRU(){
-        console.log("Translating to Russian...");
+        console.log("Перевод на россию...");
 
-        console.log("Translated... ");
+        //Navbar
+        $(".langTimeline").text("Лента новостей");
+        $(".langProject").text("Проект");
+        $(".langAccount").text("Аккаунт");
+        $(".langLogin").text("Войти");
+        $(".langRegister").text("Регистрация");
+        $(".langProfile").text("Профиль");
+        $(".langLanguage").text("Язык");
+        $(".langTheme").text("Тема");
+        $(".langSearch").text("Поиск");
+        $("#search").attr("placeholder", "Введите ответ здесь");
+
+        //Timeline
+        $(".langNewPost").text("Создать сообщение");
+        $("#newPost-textarea").attr("placeholder", "Какие новости?");
+        $(".langAddPost").text("Добавить");
+        $(".langSavePost").text("Сохранять");
+
+        //Aside
+        $(".aside-panel-title").text("Информация");
+        $(".langSavedPosts").text("Сохранено");
+
+        console.log("Переведено... ");
     }
     
 });
